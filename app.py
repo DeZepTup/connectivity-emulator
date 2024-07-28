690c0e40ad504bb6e31ed6ae756fe9b989185eb3
DeZepTup <dezeptup@gmail.com>
2024-07-28 23:11:04 +0300

    removed Server
    fix: removed Server with after_request

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from datetime import datetime, timezone
 app = Flask(__name__)
 
 
+@app.after_request
+def remove_server_header(response):
+    response.headers.pop("Server", None)
+    return response
+
+
 @app.route("/generate_204")
 def generate_204():
     response = make_response("", 204)
@@ -12,7 +18,6 @@ def generate_204():
     response.headers["Date"] = datetime.now(timezone.utc).strftime(
         "%a, %d %b %Y %H:%M:%S GMT"
     )
-    response.headers.pop("Server", None)
     response.headers.pop("Content-Type", None)
     return response
 
@@ -25,7 +30,6 @@ def connecttest():
     response.headers["Date"] = datetime.now(timezone.utc).strftime(
         "%a, %d %b %Y %H:%M:%S GMT"
     )
-    response.headers.pop("Server", None)
     return response
 
 
@@ -40,7 +44,6 @@ def captive_apple():
     response.headers["Date"] = datetime.now(timezone.utc).strftime(
         "%a, %d %b %Y %H:%M:%S GMT"
     )
-    response.headers.pop("Server", None)
     return response
 
 
@@ -51,7 +54,6 @@ def generate_204_chrome():
     response.headers["Date"] = datetime.now(timezone.utc).strftime(
         "%a, %d %b %Y %H:%M:%S GMT"
     )
-    response.headers.pop("Server", None)
     response.headers.pop("Content-Type", None)
     return response
 
@@ -64,29 +66,8 @@ def connectivity_check_ubuntu():
     response.headers["Date"] = datetime.now(timezone.utc).strftime(
         "%a, %d %b %Y %H:%M:%S GMT"
     )
-    response.headers.pop("Server", None)
     return response
 
 
-class StripServerHeaderMiddleware:
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        def custom_start_response(status, headers, exc_info=None):
-            headers = [
-                (name, value) for name, value in headers if name.lower() != "server"
-            ]
-            headers = [
-                (name, value)
-                for name, value in headers
-                if not (name.lower() == "date" and "Date" in dict(headers))
-            ]
-            return start_response(status, headers, exc_info)
-
-        return self.app(environ, custom_start_response)
-
-
 if __name__ == "__main__":
-    app.wsgi_app = StripServerHeaderMiddleware(app.wsgi_app)
     app.run(host="0.0.0.0", port=80)
